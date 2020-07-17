4dbd1a423203a04f91308240b0325282d2d222fb
Martin Kaněra <marti.kanera@gmail.com>
2020-07-17 23:24:16 +0200

    test: Google Cloud Run

--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,13 @@
     <div>
       <Logo />
       <h1 class="title">
-        @ps/website
+        UwU testík
       </h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer" class="button--green">
           Documentation
         </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer" class="button--grey">
           GitHub
         </a>
       </div>
@@ -28,9 +18,9 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
 <style>
@@ -49,16 +39,7 @@ export default Vue.extend({})
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
