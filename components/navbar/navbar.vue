ccb2143fbb3db7ec9a9f78d077cbb5ff4c6a7134
Martin Kaněra <marti.kanera@gmail.com>
2020-08-04 19:40:05 +0200

    feat(dropdown): basic dropdown

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -5,8 +5,13 @@
         .avatar-wrap
           img(src="https://i0.wp.com/www.hadviser.com/wp-content/uploads/2019/04/24-shaggy-bob-for-square-face-BcKy3nOnaAm.jpg?fit=995%2C995&ssl=1").avatar/
         span Martin Kaněra
-        drop-down(:size="28").ml-1/
-        bell(:size="22").ml-1/
+        div.flex.justify-center.items-center.relative
+          drop-down.drop(:class="{ 'active-drop': displaySettings }" @click.self="toggleSettings")/
+          ps-dropdown.left(:value="displaySettings")
+            a(href="/idk") lelelel
+        div.flex.justify-center.items-center.relative
+          bell.ml-1(@click="toggleNotifications")/
+          ps-dropdown.right(:value="displayNotifications")
     .menu-btn(@click="toggleBurger")
       .burger(:class="{ 'active': burger }")
 </template>
@@ -18,25 +23,45 @@ import bell from 'vue-material-design-icons/BellOutline.vue';
 
 export default defineComponent({
   components: { dropDown, bell },
-  props: {
-    value: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props, { emit }) {
+  setup(_, /* { emit } */ context) {
     const burger = ref(false);
 
     const toggleBurger = () => {
       burger.value = !burger.value;
-      emit('input', burger.value);
+      context.emit('input', burger.value);
     };
 
-    watchEffect(() => {
-      burger.value = props.value;
-    });
+    const displaySettings = ref(false);
 
-    return { burger, toggleBurger };
+    const toggleSettings = () => {
+      displaySettings.value = !displaySettings.value;
+      console.log(displaySettings.value);
+      if (displaySettings.value) {
+        document.addEventListener('mousedown', hideOVerlay);
+      } else {
+        document.removeEventListener('mousedown', hideOVerlay);
+      }
+    };
+
+    const displayNotifications = ref(false);
+
+    const toggleNotifications = () => (displayNotifications.value = !displayNotifications.value);
+
+    const hideOVerlay = () => {
+      displaySettings.value = false;
+      displayNotifications.value = false;
+    };
+
+    // watchEffect(() => {
+    //   // @ts-ignore
+    //   if (process.server) return;
+    //   if (displaySettings.value || displayNotifications.value) {
+    //     console.log();
+    //     document.addEventListener('mousedown', hideOVerlay, true);
+    //   }
+    // });
+
+    return { burger, toggleBurger, displaySettings, toggleSettings, displayNotifications, toggleNotifications };
   },
 });
 </script>
---
+++ components/ui/dropdown/dropdown.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  transition(name="dropdown")
+    .dropdown(v-if="display")
+      slot
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watchEffect } from 'nuxt-composition-api';
+
+export default defineComponent({
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const display = ref(false);
+
+    watchEffect(() => {
+      display.value = props.value;
+      emit('input', display.value);
+    });
+
+    return {
+      display,
+    };
+  },
+});
+</script>
+
+<style lang="sass" src="./dropdown.sass" scoped />
