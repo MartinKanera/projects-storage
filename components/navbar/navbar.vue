82e7be71fff1a7766c99d0ed472c9c8da191ab45
Martin Kaněra <marti.kanera@gmail.com>
2020-08-04 20:48:40 +0200

    feat(dropdown): added clickaway

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -5,11 +5,11 @@
         .avatar-wrap
           img(src="https://i0.wp.com/www.hadviser.com/wp-content/uploads/2019/04/24-shaggy-bob-for-square-face-BcKy3nOnaAm.jpg?fit=995%2C995&ssl=1").avatar/
         span Martin Kaněra
-        div.flex.justify-center.items-center.relative
-          drop-down.drop(:class="{ 'active-drop': displaySettings }" @click.self="toggleSettings")/
+        div.flex.justify-center.items-center.relative(v-on-clickaway="closeSettings")
+          drop-down.drop(:class="{ 'active-drop': displaySettings }" @click="toggleSettings")/
           ps-dropdown.left(:value="displaySettings")
             a(href="/idk") lelelel
-        div.flex.justify-center.items-center.relative
+        div.flex.justify-center.items-center.relative(v-on-clickaway="closeNotifications")
           bell.ml-1(@click="toggleNotifications")/
           ps-dropdown.right(:value="displayNotifications")
     .menu-btn(@click="toggleBurger")
@@ -20,48 +20,33 @@
 import { defineComponent, ref, watchEffect } from 'nuxt-composition-api';
 import dropDown from 'vue-material-design-icons/ChevronDown.vue';
 import bell from 'vue-material-design-icons/BellOutline.vue';
+import { directive as onClickaway } from 'vue-clickaway';
 
 export default defineComponent({
   components: { dropDown, bell },
-  setup(_, /* { emit } */ context) {
+  directives: { onClickaway },
+  setup(_, { emit }) {
     const burger = ref(false);
 
     const toggleBurger = () => {
       burger.value = !burger.value;
-      context.emit('input', burger.value);
+      emit('input', burger.value);
     };
 
     const displaySettings = ref(false);
 
     const toggleSettings = () => {
       displaySettings.value = !displaySettings.value;
-      console.log(displaySettings.value);
-      if (displaySettings.value) {
-        document.addEventListener('mousedown', hideOVerlay);
-      } else {
-        document.removeEventListener('mousedown', hideOVerlay);
-      }
     };
 
+    const closeSettings = () => (displaySettings.value = false);
     const displayNotifications = ref(false);
 
     const toggleNotifications = () => (displayNotifications.value = !displayNotifications.value);
 
-    const hideOVerlay = () => {
-      displaySettings.value = false;
-      displayNotifications.value = false;
-    };
+    const closeNotifications = () => (displayNotifications.value = false);
 
-    // watchEffect(() => {
-    //   // @ts-ignore
-    //   if (process.server) return;
-    //   if (displaySettings.value || displayNotifications.value) {
-    //     console.log();
-    //     document.addEventListener('mousedown', hideOVerlay, true);
-    //   }
-    // });
-
-    return { burger, toggleBurger, displaySettings, toggleSettings, displayNotifications, toggleNotifications };
+    return { burger, toggleBurger, displaySettings, closeSettings, toggleSettings, displayNotifications, toggleNotifications, closeNotifications };
   },
 });
 </script>
