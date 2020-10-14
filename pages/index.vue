e4a0b48fb877c17a2de2115954e1b098dbc3562b
Martin Kaněra <marti.kanera@gmail.com>
2020-10-14 22:09:25 +0200

    feat(modal): modal with default select

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
 .idk
+  ps-modal(:value='classModal')
+    .class-modal
+      span.text-ps-white.text-2xl Do jaké patříš třídy?
+      .form
+        select.flex-grow.md-mr-6
+          option 4.A
+          option 4.B
+        ps-btn.float-right(@click='') 
+          span.px-10 uložit
 </template>
 
 <script lang="ts">
@@ -15,12 +24,33 @@ export default defineComponent({
     if (mainStore.isStudent && mainStore.class.value === '') {
       classModal.value = true;
     }
+
+    return {
+      classModal,
+    };
   },
 });
 </script>
 
-<style>
-.idk {
-  height: 200vh;
-}
+<style lang="sass" scoped>
+.idk
+  height: 200vh
+
+.class-modal
+  @apply flex flex-col w-full h-full text-center
+
+  .form
+    @apply flex flex-col mt-4
+
+    @screen md
+      @apply flex-row justify-between items-center
+
+    select
+      @apply h-auto
+
+    .btn
+      @apply m-0 mt-3
+
+      @screen md
+        @apply m-0 ml-5
 </style>
