8ea804583404f9b639e612e1ad201b0538608315
Martin Kaněra <marti.kanera@gmail.com>
2020-10-31 14:00:21 +0100

    feat(students-page): added page

---
+++ pages/students.vue
@@ -0,0 +1,9 @@
+<template lang="pug"></template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api';
+
+export default defineComponent({
+  middleware: 'teacher',
+});
+</script>
