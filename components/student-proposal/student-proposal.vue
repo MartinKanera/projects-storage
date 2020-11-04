e00db74ba6bbc1ec6d73b06389457b8faa933fe1
Martin Kaněra <marti.kanera@gmail.com>
2020-11-04 20:28:32 +0100

    feat(students): my students section

--- components/student-proposal/student-proposal.vue
+++ components/student-proposal/student-proposal.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .student-proposal
-  .flex.idk.items-center
+  .flex.items-center
     img.border-2.border-solid.border-ps-green.rounded-full(:src='profilePicture', width='48')
     .ml-2
       span.text-ps-green.font-bold.block {{ displayName }}
--- pages/students.vue
+++ pages/students.vue
@@ -2,7 +2,7 @@
 .min-h-screen.flex.flex-col.justify-center.m-4(class='md:m-20')
   .self-start(v-if='proposals.length > 0')
     span.text-2xl.text-ps-white.font-medium Projekty ke schválení
-  .flex.flex-col.mt-4.flex-wrap.justify-between(class='md:flex-row md:justify-start')
+  .flex.flex-col.mt-4.mb-8.flex-wrap.justify-between(v-if='proposals.length > 0', class='md:flex-row md:justify-start')
     ps-student-proposal(
       v-for='proposal in proposals',
       :key='proposal.studentId',
@@ -11,6 +11,16 @@
       :projectName='proposal.projectName',
       :profilePicture='proposal.profilePicture',
       :proposalRef='proposal.proposalRef'
+    )
+  span.text-2xl.text-ps-white.font-medium(v-if='projects.length > 0') Moji studenti
+  .flex.flex-col.mt-4(class='lg:flex-row')
+    ps-teacher-project(
+      v-for='project in projects',
+      :key='project.projectId',
+      :projectId='project.projectId',
+      :projectName='project.projectName',
+      :displayName='project.displayName',
+      :profilePicture='project.profilePicture'
     )
 </template>
 
@@ -29,10 +39,20 @@ type StudentProposal = {
   proposalRef: firebase.firestore.DocumentReference;
 };
 
+type Project = {
+  projectId: String;
+  projectName: String;
+  displayName: String;
+  profilePicture: String;
+};
+
 export default defineComponent({
   middleware: 'teacher',
   setup() {
     const proposals = ref([] as Array<StudentProposal>);
+
+    const projects = ref([] as Array<Project>);
+
     const mainStore = useMainStore();
 
     if (process.client) {
@@ -68,10 +88,42 @@ export default defineComponent({
             });
           });
       } catch (_) {}
+
+      try {
+        firebase
+          .firestore()
+          .collection('projects')
+          .where('teacherId', '==', mainStore.state.user.id)
+          .where('year', '==', new Date().getFullYear())
+          .onSnapshot(async (projectSnap) => {
+            const studentIds = projectSnap.docs.map((projectDoc) => projectDoc.data().studentId);
+
+            if (!studentIds.length) {
+              projects.value = [];
+              return;
+            }
+
+            const studentsColection = (await firebase.firestore().collection('users').where(firebase.firestore.FieldPath.documentId(), 'in', studentIds).get()).docs;
+
+            projects.value = projectSnap.docs.map((projectDoc) => {
+              const currentStudent = studentsColection.find((studentDoc) => studentDoc.id === projectDoc.data().studentId);
+
+              return {
+                projectId: projectDoc.id,
+                projectName: projectDoc.data().name,
+                displayName: currentStudent?.data().displayName,
+                profilePicture: currentStudent?.data().profilePicture,
+              };
+            });
+          });
+      } catch (e) {
+        console.error(e);
+      }
     }
 
     return {
       proposals,
+      projects,
     };
   },
 });
