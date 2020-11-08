cfbfd3f85717e37b0c2b8e1a1904800960149f3f
Martin Kaněra <marti.kanera@gmail.com>
2020-11-08 14:54:19 +0100

    feat(admin): added stats component

---
+++ components/admin-stats/admin-stats.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.admin-stats
+  slot.placeholder
+  div
+    span {{ currentCount }}/
+    span.max {{ maxCount }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'nuxt-composition-api';
+
+export default defineComponent({
+  props: {
+    currentCount: {
+      type: Number,
+      default: 0,
+    },
+    maxCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+});
+</script>
+
+<style lang="sass" src="./admin-stats.sass" scoped />
