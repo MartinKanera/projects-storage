ad5d0ea9f79122b2e2013a69c3228a79dcb4804e
Martin Kaněra <marti.kanera@gmail.com>
2021-04-09 20:30:59 +0200

    feat: error page

---
+++ layouts/error-layout.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.wrapper
+  nuxt
+</template>
+
+<style lang="sass" scoped>
+.wrapper
+  @apply w-screen h-screen
+  @apply flex items-center justify-center
+  @apply text-6xl text-ps-white
+
+  background: radial-gradient(#303030, #202020)
+</style>
